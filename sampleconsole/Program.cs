54e466119164d44cf690bc84a93536eb5f0eef68
Alexander Torrijos <artorrijos@gmail.com>
2018-08-07 21:46:00 +0800

    Initial commit

---
+++ sampleconsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sampleconsole
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}

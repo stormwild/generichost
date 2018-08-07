7f97b203f73f7a7055f3eade810d6a1a71aab21e
Alexander Torrijos <artorrijos@gmail.com>
2018-08-07 23:59:26 +0800

    Updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sample Console
+
+## References
+
+- [Clean service stop by handling SIGTERM on Linux with .NET Core 2.1](https://dejanstojanovic.net/aspnet/2018/june/clean-service-stop-on-linux-with-net-core-21/)
+
+- [Having Fun with the .NET Core Generic Host](https://jmezach.github.io/2017/10/29/having-fun-with-the-.net-core-generic-host/)
+
+- [Hosting / samples](https://github.com/aspnet/Hosting/tree/38f691c09e3aae4f34be139c39ae1fb264803fd0/samples)
--- sampleconsole/Program.cs
+++ sampleconsole/Program.cs
@@ -1,16 +1,48 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using GenericHostSample;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace sampleconsole
 {
     public class Program
     {
-
         public static async Task Main(string[] args)
         {
             var host = new HostBuilder()
-                .Build();
+                .ConfigureHostConfiguration((config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.SetBasePath(Environment.CurrentDirectory);
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    config.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                    config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MyServiceA>();
+                    services.AddHostedService<MyServiceB>();
+                });
 
-            await host.RunAsync();
+            await host.RunConsoleAsync();
         }
+
+        // public static async Task Main(string[] args)
+        // {
+        //     var builder = new HostBuilder()
+        //         .ConfigureServices((hostContext, services) =>
+        //         {
+        //             services.AddHostedService<MyServiceA>();
+        //         });
+
+        //     await builder.RunConsoleAsync();
+        // }
     }
 }
